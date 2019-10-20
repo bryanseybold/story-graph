f41d24f75a02761a824da01e6ae8bfea74f4ff80
Bryan <51830722+bryanseybold@users.noreply.github.com>
2019-10-19 17:20:44 -0700

    first commit of nodes with static data and mouseover.

--- script.js
+++ script.js
@@ -26,6 +26,72 @@ function toggleTopBar() {
 	}
 }
 
+// Function to get data for this user.
+function getData() {
+	return {
+		nodes: {
+			0:{x: 30, y:50, text:"The first node"},
+			1:{x: 50, y:80, text:"The second node"},
+			2:{x: 90, y:120, text:"The third node"},
+		},
+		links: [
+			{source:0, target:1},
+			{source:2, target:1},
+		],
+	}
+}
+
+// Callback for graph node mouseover.
+function showNodeDetails(d, i) {
+	d3.select(this)
+		.transition()
+		.attr("fill", "orange")
+		.attr("r", "20px");
+	vis = d3.select("#content");
+	vis.append("text")
+		.attr("id", "node_text_" + i)
+		.attr("x", d.value.x + 10)
+		.attr("y", d.value.y)
+		.text(d.value.text);
+}
+// Callback for graph node mouseout.
+function hideNodeDetails(d, i) {
+	d3.select(this)
+		.transition()
+		.attr("fill", "black")
+		.attr("r", "10px");
+	d3.select("#node_text_" + i).remove();
+}
+
+// The main display function for nodes.
+function displayData(nodes, links) {
+	vis = d3.select("#content");
+
+	circle = vis.selectAll("circle.node")
+		.data(d3.entries(nodes), function(d){return d ? d.key : this.id;});
+	circle.enter()
+		.append("svg:circle")
+		.attr("class", "node")
+		.attr("cx", function(d) { return d.value.x; })
+		.attr("cy", function(d) { return d.value.y; })
+		.attr("r", "10px")
+		.attr("fill", "black")
+		.on("mouseover", showNodeDetails)
+		.on("mouseout", hideNodeDetails);
+	circle.exit().remove();
+
+	links = vis.selectAll(".line")
+		.data(links, function(d){ return d ? d.source + "_" + d.target : this.id;});
+	links.enter()
+		.append("line")
+		.attr("x1", function(d) { return nodes[d.source].x; })
+		.attr("y1", function(d) { return nodes[d.source].y; })
+		.attr("x2", function(d) { return nodes[d.target].x; })
+		.attr("y2", function(d) { return nodes[d.target].y; })
+		.attr("stroke", "rgb(6, 120, 155");
+	links.exit().remove();
+}
+
 // Initializes the app.
 // Handles auth redirect.
 // Sets up event handlers.
@@ -61,6 +127,9 @@ function initApp() {
 	// Listeners for top bar click events.
 	document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
 	document.getElementById('bar-toggle').addEventListener('click', toggleTopBar, false);
+
+	var data = getData();
+	displayData(data.nodes, data.links);
 }
 
 window.onload = function() {
