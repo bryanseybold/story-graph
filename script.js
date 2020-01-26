64749cb3d7a14873e22c77913cd4124aa142ed7f
Bryan <51830722+bryanseybold@users.noreply.github.com>
2020-01-25 18:46:10 -0800

    Added click and drag graph

--- script.js
+++ script.js
@@ -41,31 +41,67 @@ function getData() {
 	}
 }
 
-// Callback for graph node mouseover.
-function showNodeDetails(d, i) {
-	d3.select(this)
-		.transition()
-		.attr("fill", "orange")
-		.attr("r", "20px");
-	vis = d3.select("#content");
-	vis.append("text")
-		.attr("id", "node_text_" + i)
-		.attr("x", d.value.x + 10)
-		.attr("y", d.value.y)
-		.text(d.value.text);
+function saveData() {
+	
 }
-// Callback for graph node mouseout.
-function hideNodeDetails(d, i) {
-	d3.select(this)
-		.transition()
-		.attr("fill", "black")
-		.attr("r", "10px");
-	d3.select("#node_text_" + i).remove();
-}
+
 
 // The main display function for nodes.
 function displayData(nodes, links) {
 	vis = d3.select("#content");
+
+	// Callback for graph node mouseover.
+	function showNodeDetails(d, i) {
+		d3.select(this)
+			.transition()
+			.attr("fill", "orange")
+			.attr("r", "20px");
+		vis = d3.select("#content");
+		vis.append("text")
+			.attr("id", "node_text_" + i)
+			.attr("x", d.value.x + 10)
+			.attr("y", d.value.y)
+			.text(d.value.text);
+	}
+	// Callback for graph node mouseout.
+	function hideNodeDetails(d, i) {
+		d3.select(this)
+			.transition()
+			.attr("fill", "black")
+			.attr("r", "10px");
+		d3.select("#node_text_" + i).remove();
+	}
+
+	function dragstarted(d) {
+		d3.select(this).attr("stroke", "black");
+	}
+
+	function dragged(d) {
+		d3.select(this)
+		  .raise()
+		  .attr("cx", d.value.x = d3.event.x)
+		  .attr("cy", d.value.y = d3.event.y);
+		vis.selectAll(".line")
+		  .filter(function (dl, di) {
+			  return dl.source == d.key || dl.target == d.key;
+		  })
+   		  .attr("x1", function(dl) { return nodes[dl.source].x; })
+		  .attr("y1", function(dl) { return nodes[dl.source].y; })
+		  .attr("x2", function(dl) { return nodes[dl.target].x; })
+		  .attr("y2", function(dl) { return nodes[dl.target].y; });
+
+	}
+
+	function dragended(d) {
+		d3.select(this).attr("stroke", null);
+	}
+
+
+	function clicked(d, i) {
+		if (d3.event.defaultPrevented)
+			return; // iff dragged
+	}
+
 
 	circle = vis.selectAll("circle.node")
 		.data(d3.entries(nodes), function(d){return d ? d.key : this.id;});
@@ -77,13 +113,21 @@ function displayData(nodes, links) {
 		.attr("r", "10px")
 		.attr("fill", "black")
 		.on("mouseover", showNodeDetails)
-		.on("mouseout", hideNodeDetails);
+		.on("mouseout", hideNodeDetails)
+	        .on("click", clicked);
 	circle.exit().remove();
+
+	dragHandler = d3.drag()
+	  	 .on("start", dragstarted)
+		 .on("drag", dragged)
+		 .on("end", dragended);
+	dragHandler(vis.selectAll("circle.node"));
 
 	links = vis.selectAll(".line")
 		.data(links, function(d){ return d ? d.source + "_" + d.target : this.id;});
 	links.enter()
 		.append("line")
+		.attr("class", "line")
 		.attr("x1", function(d) { return nodes[d.source].x; })
 		.attr("y1", function(d) { return nodes[d.source].y; })
 		.attr("x2", function(d) { return nodes[d.target].x; })
@@ -118,7 +162,7 @@ function initApp() {
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			document.getElementById('sign-in').textContent = user.email + ": sign out ";
-			console.log(user);
+			//console.log(user);
 		} else {
 			document.getElementById('sign-in').textContent = "sign in";
 		}
